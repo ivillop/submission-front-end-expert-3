c3f7035c49ba0efa0431de2db870b05cb2e666a8
ivillop <rd.akbar@yandex.com>
2023-10-15 12:48:42 +0700

    Fix Webpack

--- webpack.common.js
+++ webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/webpack-demo',
+    publicPath: '/webpack-demo/',
   },
   module: {
     rules: [
